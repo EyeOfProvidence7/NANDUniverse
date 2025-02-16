0dc034841f2a2695e6118e7ce4c0dd0ff8dee93b
Said Faroghi <said.faroghi@gmail.com>
2025-02-16 06:20:35 +0100

    yes

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from itertools import product
 from nand_components import *
+
+def generate_combinations(n: int) -> list[list[int]]:
+    return [list(combination) for combination in product([0, 1], repeat=n)]
+
+n = 1
+combinations = generate_combinations(n)
+not_gate = Not()
+for combo in combinations:
+    print(f"{combo} = {not_gate.compute(combo)}")
+    print(f"Call count: {not_gate.nand_gate.call_count}")
+    not_gate.nand_gate.call_count = 0
